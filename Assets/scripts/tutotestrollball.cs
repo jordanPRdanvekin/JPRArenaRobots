badcb83a4cb16822d9b5faf36404ee09665b7604
jordanPRdanvekin <146361533+jordanPRdanvekin@users.noreply.github.com>
2025-02-27 12:16:06 +0100

    input
    arreglamos el inputsystem

--- Assets/scripts/tutotestrollball.cs
+++ Assets/scripts/tutotestrollball.cs
@@ -3,41 +3,26 @@ using UnityEngine.InputSystem;
 
 public class tutotestrollball : MonoBehaviour
 {
-    // Rigidbody of the player.
-    private Rigidbody rb;
-
-    // Movement along X and Y axes.
-    private float movementX;
-    private float movementY;
-
-    // Speed at which the player moves.
-    public float speed = 0;
-
-    // Start is called before the first frame update.
+    private Rigidbody BolaRB;
+    [SerializeField] private float moverX;
+    [SerializeField] private float moverY;
+    [SerializeField] public float vel = 10;
     void Start()
     {
-        // Get and store the Rigidbody component attached to the player.
-        rb = GetComponent<Rigidbody>();
+        BolaRB = GetComponent<Rigidbody>();
     }
-
-    // This function is called when a move input is detected.
-    void OnMove(InputValue movementValue)
+     public void OnMove(InputValue movementValue)
     {
-        // Convert the input value into a Vector2 for movement.
-        Vector2 movementVector = movementValue.Get<Vector2>();
-
-        // Store the X and Y components of the movement.
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        Vector2 vectormover = movementValue.Get<Vector2>();
+        moverX = vectormover.x;
+        moverY = vectormover.y;
     }
-
-    // FixedUpdate is called once per fixed frame-rate frame.
     private void FixedUpdate()
     {
-        // Create a 3D movement vector using the X and Y inputs.
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        Vector3 moviendo = new Vector3(moverX, 0.0f, moverY);
+        BolaRB.AddForce(moviendo * vel);
 
-        // Apply force to the Rigidbody to move the player.
-        rb.AddForce(movement * speed);
     }
+
+
 }
